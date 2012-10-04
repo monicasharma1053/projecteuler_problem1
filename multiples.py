ce33127954f9829ae03cd07e8b2f5494842506c4
Monica Sharma <monicasharma1053@gmail.com>
2012-10-04 15:31:30 -0500

    multiplestests

--- multiples.py
+++ multiples.py
@@ -3,3 +3,18 @@
 
 
 class Multiples: 
+
+    global numberOfMultiples
+    global multiplier
+    global multiplicand
+    
+
+    def MultiplesInARange(self, multiplier, numberOfMultiples):
+        multiplicand=1
+        multipleslist = list() 
+        while(multiplicand<numberOfMultiples):
+            product=multiplier*multiplicand
+            multipleslist.append(product+1) 
+            multiplicand+1
+        return l
+        
--- multiplestest.py
+++ multiplestest.py
@@ -3,8 +3,21 @@ from multiples import Multiples
 import unittest
 
 
-class IsMultiplesTest(unittest.TestCase):
+class MultiplesTest(unittest.TestCase):
 
 
 
-    
+    def setUp(self):
+        self.m=Multiples()
+
+
+    def testOneFourMultiplesRange(self):
+        self.assertEqual([1,2,3,4], self.m.MultiplesInARange(1,4)) 
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
